978ca216bcc37dcf75c416aad1718170899f18ac
Maciej Zając <maciej.zajac.197@gmail.com>
2019-11-08 23:53:27 +0100

    project initialization, prepared redux boilerplate and scss

---
+++ src/components/header/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+
+export default class Header extends Component {
+    render() {
+        return (
+            <nav>
+                <Link to='/'>Home</Link>
+                <Link to='/post-offer'>Post a job</Link>
+                <Link to='/login'>Log in</Link>
+            </nav>
+        );
+    }
+}
---
+++ src/router.js
@@ -0,0 +1,20 @@
+import React from "react";
+import App from "./App";
+import Layout from "./components/HOC/Layout";
+import { Router as BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { createBrowserHistory } from "history";
+const history = createBrowserHistory();
+const Router = () => {
+    return (
+        <BrowserRouter history={history}>
+            <Layout>
+                <Switch>
+                    <Route exact='/' component={App} />
+                    <Route exact='/home' component={App} />
+                </Switch>
+            </Layout>
+        </BrowserRouter>
+    );
+};
+export default Router;
